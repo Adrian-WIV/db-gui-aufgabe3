3b12c7f72dd881df8ac243677cd36d605054b96a
Adrian-WIV <adrian.badar@rdf.nuernberg.de>
2025-04-15 09:38:56 +0200

    abfrage erstellt

--- db.py
+++ db.py
@@ -15,12 +15,14 @@ from tkinter import messagebox
 #klasse
 
 class User:
-    def __init__(self, anrede, vorname, name, tel):
+    def __init__(self, anrede, vorname, name, tel, alter):
         self.anrede = anrede
         self.vorname = vorname
         self.name = name
         self.tel = tel
+        self.alter = alter
 
+#mariadb verbinden
 def mariadbconnect():
     try: 
         conn = mariadb.connect(
@@ -30,8 +32,34 @@ def mariadbconnect():
             port = 3306,
             database = "schlumpfshop3"
         )
-
+    
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB: {e}")
 
     cur = conn.cursor()
+
+    return cur, conn
+
+#db abfrage
+def sql(cur, eingabe):
+
+    cur.execute("""SELECT 
+  anrede.Anrede, 
+  kunden.Vorname, 
+  kunden.Name, 
+  kunden.Telefon,
+  TIMESTAMPDIFF(YEAR, kunden.Geburtsdatum, CURDATE()) AS 'Alter'
+FROM kunden
+JOIN anrede ON kunden.Anrede = `anrede`.`ID_Anrede`
+WHERE TIMESTAMPDIFF(YEAR, kunden.Geburtsdatum, CURDATE()) < ?
+""", (eingabe,))
+    
+    ergebnis = cur.fetchall()
+
+    kunden_liste = []
+
+    for e in ergebnis:
+        kunde = User(*e)
+        kunden_liste.append(kunde)
+
+    return kunden_liste
